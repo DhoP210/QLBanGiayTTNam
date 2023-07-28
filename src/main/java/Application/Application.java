14d3916d15e9e0a8b72e5bb965873b283dda7983
LAPTOP-U7MQ6KHT\ASUS <hoppdph23992@fpt.edu.vn>
2023-07-28 17:00:59 +0700

    update

---
+++ src/main/java/Application/Application.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Application;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("acb");
+        System.out.println("Thành Đẹp Trai");
+        System.out.println("cde");
+        System.out.println("hop");
+        System.out.println("hamoi");
+        System.out.println("let's check");
+    }
+    
+}
